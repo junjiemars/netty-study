6cfbc24e4014871c36eef301377407f5d3ad11f5
南山 <junjiemars@gmail.com>
2015-03-09 17:13:47 +0800

    it just can access http://cn.bing.com, next move:http+xml

--- clients/src/main/java/HttpXmlClient.java
+++ clients/src/main/java/HttpXmlClient.java
@@ -1,9 +1,11 @@
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.http.*;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 
 import java.net.URI;
 
@@ -12,44 +14,70 @@ import java.net.URI;
  */
 public class HttpXmlClient {
     public static void main(String[] args) throws Exception {
-    URI uri = new URI(args[0]);
-    String host = uri.getHost() == null? "127.0.0.1" : uri.getHost();
-    int port = uri.getPort() == -1 ? 8080 : uri.getPort();
+        URI uri = new URI("http://cn.bing.com");
+        String scheme = uri.getScheme() == null? "http" : uri.getScheme();
+        String host = uri.getHost() == null? "127.0.0.1" : uri.getHost();
+        int port = uri.getPort();
+        if (port == -1) {
+            if ("http".equalsIgnoreCase(scheme)) {
+                port = 80;
+            } else if ("https".equalsIgnoreCase(scheme)) {
+                port = 443;
+            }
+        }
 
-     // Configure the client.
-    EventLoopGroup group = new NioEventLoopGroup();
-    try {
-      Bootstrap b = new Bootstrap();
-      b.group(group)
-          .channel(NioSocketChannel.class)
-      .handler(new HttpXmlClientHandler());
-          //.handler(new HttpSnoopClientInitializer(sslCtx));
+        if (!"http".equalsIgnoreCase(scheme) && !"https".equalsIgnoreCase(scheme)) {
+            System.err.println("Only HTTP(S) is supported.");
+            return;
+        }
 
-      // Make the connection attempt.
-      Channel ch = b.connect(host, port).sync().channel();
+        // Configure SSL context if necessary.
+        final boolean ssl = "https".equalsIgnoreCase(scheme);
+        final SslContext sslCtx;
+        if (ssl) {
+            sslCtx = SslContext.newClientContext(InsecureTrustManagerFactory.INSTANCE);
+        } else {
+            sslCtx = null;
+        }
 
-      // Prepare the HTTP request.
-      HttpRequest request = new DefaultFullHttpRequest(
-          HttpVersion.HTTP_1_1, HttpMethod.GET, uri.getRawPath());
-      request.headers().set(HttpHeaderNames.HOST, host);
-      request.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.CLOSE);
-      request.headers().set(HttpHeaderNames.ACCEPT_ENCODING, HttpHeaderValues.GZIP);
+        // Configure the client.
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new HttpXmlClientInitializer(sslCtx));
 
-      // Set some example cookies.
-      request.headers().set(
-          HttpHeaderNames.COOKIE,
-          ClientCookieEncoder.encode(
-              new DefaultCookie("my-cookie", "foo"),
-              new DefaultCookie("another-cookie", "bar")));
+            // Make the connection attempt.
+            Channel ch = b.connect(host, port).sync().channel();
 
-      // Send the HTTP request.
-      ch.writeAndFlush(request);
+            // Prepare the HTTP request.
+            HttpRequest request = new DefaultFullHttpRequest(
+                    HttpVersion.HTTP_1_1, HttpMethod.GET, uri.getRawPath());
+            request.headers().set(HttpHeaderNames.HOST, host);
+            request.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.CLOSE);
+            request.headers().set(HttpHeaderNames.ACCEPT_ENCODING, HttpHeaderValues.GZIP);
 
-      // Wait for the server to close the connection.
-      ch.closeFuture().sync();
-    } finally {
-      // Shut down executor threads to exit.
-      group.shutdownGracefully();
-    }
+            // Set some example cookies.
+            request.headers().set(
+                    HttpHeaderNames.COOKIE,
+                    ClientCookieEncoder.encode(
+                            new DefaultCookie("my-cookie", "foo"),
+                            new DefaultCookie("another-cookie", "bar")));
+
+            // Send the HTTP request.
+            ch.writeAndFlush(request);
+
+            // Wait for the server to close the connection.
+            ch.closeFuture().sync();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        finally {
+            // Shut down executor threads to exit.
+            group.shutdownGracefully();
+        }
   }
 }
+
+
--- clients/src/main/java/HttpXmlClientHandler.java
+++ clients/src/main/java/HttpXmlClientHandler.java
@@ -14,40 +14,40 @@ public class HttpXmlClientHandler extends SimpleChannelInboundHandler<HttpObject
     ctx.close();
   }
 
-  @Override
-  protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
-    if (msg instanceof HttpResponse) {
-      HttpResponse response = (HttpResponse) msg;
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, HttpObject msg) {
+        if (msg instanceof HttpResponse) {
+            HttpResponse response = (HttpResponse) msg;
 
-      System.err.println("STATUS: " + response.status());
-      System.err.println("VERSION: " + response.protocolVersion());
-      System.err.println();
+            System.err.println("STATUS: " + response.status());
+            System.err.println("VERSION: " + response.protocolVersion());
+            System.err.println();
 
-      if (!response.headers().isEmpty()) {
-        for (CharSequence name: response.headers().names()) {
-          for (CharSequence value: response.headers().getAll(name)) {
-            System.err.println("HEADER: " + name + " = " + value);
-          }
+            if (!response.headers().isEmpty()) {
+                for (CharSequence name: response.headers().names()) {
+                    for (CharSequence value: response.headers().getAll(name)) {
+                        System.err.println("HEADER: " + name + " = " + value);
+                    }
+                }
+                System.err.println();
+            }
+
+            if (HttpHeaderUtil.isTransferEncodingChunked(response)) {
+                System.err.println("CHUNKED CONTENT {");
+            } else {
+                System.err.println("CONTENT {");
+            }
         }
-        System.err.println();
-      }
+        if (msg instanceof HttpContent) {
+            HttpContent content = (HttpContent) msg;
 
-      if (HttpHeaderUtil.isTransferEncodingChunked(response)) {
-        System.err.println("CHUNKED CONTENT {");
-      } else {
-        System.err.println("CONTENT {");
-      }
+            System.err.print(content.content().toString(CharsetUtil.UTF_8));
+            System.err.flush();
+
+            if (content instanceof LastHttpContent) {
+                System.err.println("} END OF CONTENT");
+                ctx.close();
+            }
+        }
     }
-    if (msg instanceof HttpContent) {
-      HttpContent content = (HttpContent) msg;
-
-      System.err.print(content.content().toString(CharsetUtil.UTF_8));
-      System.err.flush();
-
-      if (content instanceof LastHttpContent) {
-        System.err.println("} END OF CONTENT");
-        ctx.close();
-      }
-    }
-  }
 }
